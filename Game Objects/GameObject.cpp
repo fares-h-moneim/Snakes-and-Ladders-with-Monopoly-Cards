6bb44ee0df86ff92a8c8ba2668bc2cd3284793cb
Fares Hesham <66084853+fares-h-moneim@users.noreply.github.com>
2022-01-11 11:29:04 +0200

    Different Game Object Classes
    Classes for Cards, Snake, Ladder

---
+++ Game Objects/GameObject.cpp
@@ -0,0 +1,38 @@
+#include "GameObject.h"
+#include "Ladder.h"
+#include "Snake.h"
+#include "Card.h"
+
+GameObject::GameObject(const CellPosition & pos)
+{
+	position = pos; // Sets Position
+}
+
+CellPosition GameObject::GetPosition() const
+{
+	return position;
+}
+
+void GameObject::SetPosition(CellPosition pos) {
+	position = pos;
+}
+
+CellPosition GameObject::GetEndPosition() const{
+	return position;
+}
+
+CellPosition GameObject::GetStartPosition() const{
+	return position;
+}
+
+int GameObject::GetStartNum() const{
+	return position.GetCellNum();
+}
+
+int GameObject::GetEndNum() const{
+	return position.GetCellNum();
+}
+
+GameObject::~GameObject()
+{
+}
